0b72a1e6bb3d612dc79d564e327c7e278ce9fccb
willnet <netwillnet@gmail.com>
2016-10-03 00:42:04 +0900

    Enable process images on upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,17 @@
+require 'image_processing/mini_magick'
+
 class ImageUploader < Shrine
+  include ImageProcessing::MiniMagick
+
   plugin :validation_helpers
+  plugin :processing
 
   Attacher.validate do
     validate_max_size 5.megabytes
     validate_mime_type_inclusion ['image/jpeg']
+  end
+
+  process(:store) do |io, context|
+    resize_to_limit(io.download, 700, 700)
   end
 end
