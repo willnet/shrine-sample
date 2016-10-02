fd9515ede200270ac89d795aca9f211e61375d6d
willnet <netwillnet@gmail.com>
2016-10-03 00:18:23 +0900

    Add global settings for shrine

---
+++ config/initializers/shrine.rb
@@ -0,0 +1,14 @@
+require 'shrine'
+require 'shrine/storage/file_system'
+
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new(
+    'public', prefix: 'uploads/cache'
+  ),
+  store: Shrine::Storage::FileSystem.new(
+    'public', prefix: 'uploads/store'
+  )
+}
+
+Shrine.plugin :cached_attachment_data
+Shrine.plugin :activerecord
